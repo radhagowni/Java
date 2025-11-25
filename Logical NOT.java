4f9e921bbae5ee8d2d3d58e82498a32969ec481f
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-11-25 18:06:48 +0530

    Initial commit

---
+++ Logical NOT.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+      Boolean flag=true;
+      if(flag)
+      {
+        System.out.println("The Flag value is true.");
+      }
+      else
+      {
+        System.out.println("The Flag value is false.");
+
+      }
+        System.out.println("I am out of the block..");
+
+    }
+}
