fbdab0e14e96020e23bc57e4896f45c49f7bbd41
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-11-26 16:15:06 +0530

    first commit

---
+++ Equal Product Checker.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter three values:");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        if((a*b)==c)
+        {
+         System.out.println("true");
+        }
+        else{
+         System.out.println("false");
+       
+        }
+    }
+}
